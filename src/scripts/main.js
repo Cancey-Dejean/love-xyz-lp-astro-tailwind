47e7b4eca15ba00a73815284d53fca49bd3a0884
Cancey-Dejean <you@example.com>
2023-09-21 18:16:42 -0500

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const jamesText = document.querySelector(".lj-text")
+  const jamesLoveText = document.querySelector(".lj-love")
+  const jamesImg = document.querySelector(".lj-img")
+  const songTextLetters = document.querySelectorAll(".song-text span")
+  const mainVideo = document.querySelector(".main-video")
+
+  const tl = gsap.timeline()
+
+  tl.from(jamesImg, { opacity: 0.4, duration: 1.3, scale: 1.4, delay: 1 })
+    .from(jamesText, { y: 70, scale: 1.5, opacity: 0, duration: 1.5 }, "-=.2")
+    .from(jamesLoveText, { opacity: 0 }, "-=.2")
+    .from(songTextLetters, {
+      duration: 1.5,
+      opacity: 0,
+      y: 50,
+      stagger: 0.2, // Stagger the animation of the span elements
+      delay: 0.1,
+    })
+    .from(mainVideo, { opacity: 0, duration: 2 })
+})
